5c5d077e8f70f0420e6d0c946c2a2e5e71e8c2ca
DrMabuse23 <p.brewing@mwaysolutions.com>
2015-07-01 15:39:57 +0200

    Update 2015-07-01T13:39:54.540Z

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -1,6 +1,6 @@
 NG_DOCS={
   "sections": {
-    "api": "Relution Client Security - 1.0.1"
+    "api": "Relution Client Security - 1.0.2"
   },
   "pages": [
     {
@@ -70,7 +70,7 @@ NG_DOCS={
       "js/prettify.js"
     ],
     "styles": [],
-    "title": "Relution Client Security - 1.0.1",
+    "title": "Relution Client Security - 1.0.2",
     "html5Mode": true,
     "editExample": true,
     "navTemplate": false,
